c29346a75596efc6b7eec984d952831a83eafcd4
portido5gusto <portido5gusto@gmail.com>
2026-06-30 13:18:06 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = "portido-magazzino-v1";
+const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
+});
